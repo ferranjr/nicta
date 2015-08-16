9f0f60cc9799953466342aeece44a06ba270bc22
Ferran Puig Calvache <ferran.jr@gmail.com>
2015-08-16 15:30:09 +0100

    Apply tests passed

--- src/Course/Apply.hs
+++ src/Course/Apply.hs
@@ -31,24 +31,16 @@ infixl 4 <*>
 -- >>> Id (+10) <*> Id 8
 -- Id 18
 instance Apply Id where
-  (<*>) ::
-    Id (a -> b)
-    -> Id a
-    -> Id b
-  (<*>) =
-    error "todo: Course.Apply (<*>)#instance Id"
+  (<*>) :: Id (a -> b) -> Id a -> Id b
+  Id f <*> Id(a) = Id( f a )
 
 -- | Implement @Apply@ instance for @List@.
 --
 -- >>> (+1) :. (*2) :. Nil <*> 1 :. 2 :. 3 :. Nil
 -- [2,3,4,2,4,6]
 instance Apply List where
-  (<*>) ::
-    List (a -> b)
-    -> List a
-    -> List b
-  (<*>) =
-    error "todo: Course.Apply (<*>)#instance List"
+  (<*>) :: List (a -> b) -> List a -> List b
+  f <*> a = flatMap (`map` a ) f
 
 -- | Implement @Apply@ instance for @Optional@.
 --
@@ -61,12 +53,8 @@ instance Apply List where
 -- >>> Full (+8) <*> Empty
 -- Empty
 instance Apply Optional where
-  (<*>) ::
-    Optional (a -> b)
-    -> Optional a
-    -> Optional b
-  (<*>) =
-    error "todo: Course.Apply (<*>)#instance Optional"
+  (<*>) :: Optional (a -> b) -> Optional a -> Optional b
+  f <*> a = bindOptional ( `mapOptional` a ) f
 
 -- | Implement @Apply@ instance for reader.
 --
@@ -85,12 +73,8 @@ instance Apply Optional where
 -- >>> ((*) <*> (+2)) 3
 -- 15
 instance Apply ((->) t) where
-  (<*>) ::
-    ((->) t (a -> b))
-    -> ((->) t a)
-    -> ((->) t b)
-  (<*>) =
-    error "todo: Course.Apply (<*>)#instance ((->) t)"
+  (<*>) :: ((->) t (a -> b)) -> ((->) t a) -> ((->) t b)
+  f <*> g = \x -> f x ( g x )
 
 -- | Apply a binary function in the environment.
 --
@@ -111,14 +95,8 @@ instance Apply ((->) t) where
 --
 -- >>> lift2 (+) length sum (listh [4,5,6])
 -- 18
-lift2 ::
-  Apply f =>
-  (a -> b -> c)
-  -> f a
-  -> f b
-  -> f c
-lift2 =
-  error "todo: Course.Apply#lift2"
+lift2 :: Apply f => (a -> b -> c) -> f a -> f b -> f c
+lift2 f a b = f <$> a <*> b
 
 -- | Apply a ternary function in the environment.
 --
@@ -142,15 +120,8 @@ lift2 =
 --
 -- >>> lift3 (\a b c -> a + b + c) length sum product (listh [4,5,6])
 -- 138
-lift3 ::
-  Apply f =>
-  (a -> b -> c -> d)
-  -> f a
-  -> f b
-  -> f c
-  -> f d
-lift3 =
-  error "todo: Course.Apply#lift2"
+lift3 :: Apply f => (a -> b -> c -> d) -> f a -> f b -> f c -> f d
+lift3 f a b c = f <$> a <*> b <*> c
 
 -- | Apply a quaternary function in the environment.
 --
@@ -174,16 +145,8 @@ lift3 =
 --
 -- >>> lift4 (\a b c d -> a + b + c + d) length sum product (sum . filter even) (listh [4,5,6])
 -- 148
-lift4 ::
-  Apply f =>
-  (a -> b -> c -> d -> e)
-  -> f a
-  -> f b
-  -> f c
-  -> f d
-  -> f e
-lift4 =
-  error "todo: Course.Apply#lift4"
+lift4 :: Apply f => (a -> b -> c -> d -> e) -> f a -> f b -> f c -> f d -> f e
+lift4 f a b c d = f <$> a <*> b <*> c <*> d
 
 -- | Sequence, discarding the value of the first argument.
 -- Pronounced, right apply.
@@ -203,13 +166,8 @@ lift4 =
 -- prop> [a,b,c] *> [x,y,z] == [x,y,z,x,y,z,x,y,z]
 --
 -- prop> Full x *> Full y == Full y
-(*>) ::
-  Apply f =>
-  f a
-  -> f b
-  -> f b
-(*>) =
-  error "todo: Course.Apply#(*>)"
+(*>) :: Apply f => f a -> f b -> f b
+(*>) = lift2(const id)
 
 -- | Sequence, discarding the value of the second argument.
 -- Pronounced, left apply.
@@ -230,12 +188,9 @@ lift4 =
 --
 -- prop> Full x <* Full y == Full x
 (<*) ::
-  Apply f =>
-  f b
-  -> f a
-  -> f b
+  Apply f => f b -> f a -> f b
 (<*) =
-  error "todo: Course.Apply#(<*)"
+  lift2 const
 
 -----------------------
 -- SUPPORT LIBRARIES --
